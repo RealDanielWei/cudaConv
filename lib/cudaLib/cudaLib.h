a1b1b6db8c9ea7c89a00f30467ced8b7d1aaa588
Zhangchao Wei <zhangchao.wei@formlabs.com>
2021-04-18 18:55:56 -0400

    Naive kernel

--- lib/cudaLib/cudaLib.h
+++ lib/cudaLib/cudaLib.h
@@ -1,2 +1,7 @@
-
+#include <functional>
 void cudaFunction();
+
+void Convolution2D(float* h_in, float* h_out, int width, int height, float* h_kernel, int kernelWidth, int kernelHeight);
+
+using kernelCall = std::function<void()>;
+float timing_experiment(kernelCall func);
